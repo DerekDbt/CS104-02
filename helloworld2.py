f2c244e36450487bfa8394fb57720210066a737e
DerekDbt <114094053+DerekDbt@users.noreply.github.com>
2022-09-26 11:54:33 -0400

    Update helloworld2.py

--- helloworld2.py
+++ helloworld2.py
@@ -1,3 +1,4 @@
 Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> # This is Version 2 of my hello world program
+# This is Version 2 of my hello world program
+print("Hello my name is Derek Boyle") 
